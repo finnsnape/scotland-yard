babe1dbe92198b2daee31200b5eda3dabe1e3a5d
Hellin <sun>
2021-03-17 11:00:30 +0000

    3tests

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,8 +4,12 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * cw-model
@@ -18,8 +22,46 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			Player mrX,
 			ImmutableList<Player> detectives) {
 		// TODO
-		throw new RuntimeException("Implement me!");
+		return new MyGameState(setup, ImmutableSet.of(Piece.MrX.MRX), ImmutableList.of(), mrX, detectives);
 
 	}
+
+
+private final class MyGameState implements GameState {
+
+	private GameSetup setup;
+	private ImmutableSet<Piece> remaining;
+	private ImmutableList<LogEntry> log;
+	private Player mrX;
+	private List<Player> detectives;
+	private ImmutableList<Player> everyone;
+	private ImmutableSet<Move> moves;
+	private ImmutableSet<Piece> winner;
+
+	private MyGameState(
+			final GameSetup setup,
+			final ImmutableSet<Piece> remaining,
+			final ImmutableList<LogEntry> log,
+			final Player mrX,
+			final List<Player> detectives
+	) {
+		this.setup = setup;
+		this.remaining = remaining;
+		this.log = log;
+		this.mrX = mrX;
+		this.detectives = detectives;
+		//
+	}
+
+
+
+	@Override public GameSetup getSetup(){ return null; }
+	@Override public ImmutableSet<Piece> getPlayers() { return null; }
+	@Override public Optional<Integer> getDetectiveLocation(Piece.Detective detective) { return null;}
+	@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {return null;}
+	@Override public ImmutableList<LogEntry> getMrXTravelLog() { return null; }
+	@Override public ImmutableSet<Piece> getWinner(){ return null; }
+	@Override public ImmutableSet<Move> getAvailableMoves() {return null; }
+	@Override public GameState advance(Move move) { return null; }
 
 }
